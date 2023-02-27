b2dd13dca71480073c20356b33fa1bea522b22aa
Md Abu Sayed <sayedmdabu94@gmail.com>
2023-02-27 18:51:37 +0600

    Learn 27-02-2023

--- server.js
+++ server.js
@@ -5,15 +5,42 @@ const usersData = require('./users.json');
 
 const app = express();
 // Construct a schema, using GraphQL schema language
+
+let fakeDb = [
+  {id:1, name:"Office", rent: "$25"},
+  {id:2, name:"co-working", rent: "$10"},
+];
+
 const schema = buildSchema(`
   type Person {
     name: String,
-    email: String
+    email: String,
+    id: Int
   }
 
   type Query {
     users: [Person],
     user(id: Int): Person
+    getMsg: String
+    getSpace(id:ID!) : Space!
+    
+  }
+
+  type Space {
+    name: String
+    rent: String
+  }
+
+  input SpaceInput {
+    name: String
+    rent: String
+  }
+
+
+  type Mutation {
+    addMsg(msg: String): String
+    createSpace(input: SpaceInput): Space!
+    updateSpace(id:ID!, input:SpaceInput):Space!
   }
 `);
 
@@ -21,6 +48,17 @@ const schema = buildSchema(`
 const root = {
   users: () => usersData,
   user:({id}) => usersData.find((user) => user.id === id),
+  addMsg: ({msg}) => (fakeDb.message = msg),
+  getMsg: () => fakeDb.message,
+  createSpace: ({input}) => (fakeDb[fakeDb.length] = {id:fakeDb.length, name:input.name, rent:input.rent}),
+  getSpace: ({id}) => fakeDb.find(space => space.id == id),
+
+  updateSpace: ({id, input}) =>{
+    const index = id-1;
+    fakeDb[index] = {id, name:input.name, rent:input.rent}
+    return fakeDb[index]
+  },
+
 };
 
 // fetch('/graphql', {
